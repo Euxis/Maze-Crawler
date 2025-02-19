307cd94b8397ab0c5965d9f1dfa839f1e99422bf
Euxis <aidenmai476@gmail.com>
2025-02-19 17:36:10 -0500

    add prim maze generator

---
+++ Assets/Scripts/MazeGenerate.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MazeGenerate : MonoBehaviour
+{
+    /*
+     * Code based on Apolok99's Unity Maze Generator: https://github.com/Apolok99/Maze-Random-Generator-Unity
+     * Based on Prim's algorithm.
+     */
+    
+    private const bool Wall = true;
+    private const bool Passage = false;
+
+    public GameObject prefabWall;
+    public int mazeWidth;
+    public int mazeHeight;
+
+    // Two dimensional array for grid and structure
+    private bool[,] _mazeGrid;
+
+    private GameObject[,] _mazeStructure;
+
+    void Start()
+    {
+        GenerateMaze();
+    }
+
+    private void GenerateMaze()
+    {
+        _mazeGrid = new bool[mazeWidth, mazeHeight];
+        _mazeStructure = new GameObject[mazeWidth, mazeHeight];
+
+        for (int x = 0; x < mazeWidth; x++)
+        {
+            for (int y = 0; y < mazeHeight; y++)
+            {
+                _mazeGrid[x, y] = Wall;
+                _mazeStructure[x, y] = Instantiate(prefabWall, new Vector2(x * 1f, y * 1f), Quaternion.identity,
+                    GetComponent<Transform>());
+            }
+        }
+        
+        // Select a random cell to be the starting point
+        int startCellX = Random.Range(3, mazeWidth - 3);
+        int startCellY = Random.Range(3, mazeHeight - 3);
+        _mazeGrid[startCellX, startCellY] = Passage;
+
+        HashSet<(int, int)> frontierCells = GetNeightbourCells(startCellX, startCellY, true);
+        
+        // While there's frontier cells, continue creating new passages
+        while (frontierCells.Any())
+        {
+            // Select a random frontier cell and change it into a passage
+            int randomIndex = Random.Range(0, frontierCells.Count);
+            (int, int) randomFrontierCell = frontierCells.ElementAt(randomIndex);
+            int randomFrontierCellX = randomFrontierCell.Item1;
+            int randomFrontierCellY = randomFrontierCell.Item2;
+            _mazeGrid[randomFrontierCellX, randomFrontierCellY] = Passage;
+
+            // Obtain the list of passage cells which can be connected to the selected frontier cell
+            HashSet<(int, int)> candidateCells = GetNeightbourCells(randomFrontierCellX, randomFrontierCellY, false);
+            if (candidateCells.Any())
+            {
+                // Select a random passage to connect with the frontier cell
+                int randomIndexCandidate = Random.Range(0, candidateCells.Count);
+                (int, int) randomCellConnection = candidateCells.ElementAt(randomIndexCandidate);
+                int randomCellConnectionX = randomCellConnection.Item1;
+                int randomCellConnectionY = randomCellConnection.Item2;
+                
+                // Calculate which cell is inbetween the frontier cell and the passage
+                (int, int) cellBetween;
+                if (randomFrontierCellX < randomCellConnectionX)
+                    cellBetween = (randomFrontierCellX + 1, randomFrontierCellY);
+                else if (randomFrontierCellX > randomCellConnectionX)
+                    cellBetween = (randomFrontierCellX - 1, randomFrontierCellY);
+                else
+                {
+                    if (randomFrontierCellY < randomCellConnectionY)
+                        cellBetween = (randomFrontierCellX, randomFrontierCellY + 1);
+                    else
+                        cellBetween = (randomFrontierCellX, randomFrontierCellY - 1);
+                }
+                
+                // Make the cell in between a passage to connect the frontier and passage cell
+                _mazeGrid[cellBetween.Item1, cellBetween.Item2] = Passage;
+            }
+            // remove the frontier cell that has been converted to a passage
+            frontierCells.Remove(randomFrontierCell);
+            
+            frontierCells.UnionWith(GetNeightbourCells(randomFrontierCellX, randomFrontierCellY, true));
+        }
+        
+        // Deactivate the GameObjects of the walls that are a passage
+        for (int x = 0; x < mazeWidth; x++)
+        {
+            for (int y = 0; y < mazeHeight; y++)
+            {
+                if (_mazeGrid[x, y] == Passage)
+                {
+                    _mazeStructure[x,y].SetActive(false);
+                }
+            }
+        }
+    }
+
+    private HashSet<(int, int)> GetNeightbourCells(int x, int y, bool checkFrontier)
+    {
+        HashSet<(int, int)> newNeighbourCells = new HashSet<(int, int)>();
+        
+        // Check if the cell can have a neighbour cell
+        if (x > 2)
+        {
+            (int, int) cellToCheck = (x - 2, y);
+
+            if (checkFrontier
+                    ? _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Wall
+                    : _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Passage)
+            {
+                newNeighbourCells.Add(cellToCheck);
+            }
+        }
+        if (x < mazeWidth - 3)
+        {
+            (int, int) cellToCheck = (x + 2, y);
+            if (checkFrontier
+                    ? _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Wall
+                    : _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Passage)
+            {
+                newNeighbourCells.Add(cellToCheck);
+            }
+        }
+
+        if (y > 2)
+        {
+            (int, int) cellToCheck = (x, y - 2);
+            if (checkFrontier
+                    ? _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Wall
+                    : _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Passage)
+            {
+                newNeighbourCells.Add(cellToCheck);
+            }
+        }
+
+        if (y < mazeHeight - 3)
+        {
+            (int, int) cellToCheck = (x, y + 2);
+            if (checkFrontier
+                    ? _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Wall
+                    : _mazeGrid[cellToCheck.Item1, cellToCheck.Item2] == Passage)
+            {
+                newNeighbourCells.Add(cellToCheck);
+            }
+        }
+        return newNeighbourCells;
+    }
+}
